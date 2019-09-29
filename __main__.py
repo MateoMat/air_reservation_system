29044e4e41262e62f8600b1e60c64bf90f22fbf0
Mateusz Maslanka <m.maslank@interia.pl>
2019-09-29 12:01:07 +0200

    creating class structure in folders

---
+++ __main__.py
@@ -0,0 +1,1 @@
+# Informacja dotycząca całej aplikacji
---
+++ aircraft/aircraft.py
@@ -0,0 +1,5 @@
+class Aircraft:
+    def num_seats(self):
+        rows, row_seats = self.seating_plan()
+        return len(rows) * len(row_seats)
+
